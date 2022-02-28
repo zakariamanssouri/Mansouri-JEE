85bcb8b99a22981802bdc3d57bf0727100b58ddc
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-02-28 07:17:37 -0500

    ajouter une instruction dans DaoImpl

--- InjectionDesDépendances/src/main/java/dao/DaoImpl.java
+++ InjectionDesDépendances/src/main/java/dao/DaoImpl.java
@@ -2,6 +2,7 @@ package dao;
 
 public class DaoImpl implements IDAO{
     public double getdata() {
+        System.out.println("version base de données");
         return Math.PI*100/Math.cos(10);
     }
 }

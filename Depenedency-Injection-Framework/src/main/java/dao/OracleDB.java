e255b847410eab42ae548661a0f7e475f6852c4b
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-03-26 20:08:30 +0100

    Création d'une implémentation de l'interface Database

---
+++ Depenedency-Injection-Framework/src/main/java/dao/OracleDB.java
@@ -0,0 +1,17 @@
+package dao;
+
+
+
+public class OracleDB implements Database {
+
+    public OracleDB() {
+    }
+
+    public String getUserData(String username) {
+        System.out.println("getting your info from oracle database");
+        System.out.println("wait...");
+        System.out.println("success");
+        System.out.println("Username is : "+username);
+        return username;
+    }
+}

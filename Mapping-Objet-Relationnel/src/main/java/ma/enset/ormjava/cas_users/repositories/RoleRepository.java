939b9078ec7e16eb14379aa0bc51edcaa2ec1a81
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-01 19:04:13 +0000

    Création des interfaces UserRepository et RoleRepository

---
+++ Mapping-Objet-Relationnel/src/main/java/ma/enset/ormjava/cas_users/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package ma.enset.ormjava.cas_users.repositories;
+
+import ma.enset.ormjava.cas_users.entities.Role;
+import ma.enset.ormjava.cas_users.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
---
+++ Mapping-Objet-Relationnel/src/main/java/ma/enset/ormjava/cas_users/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package ma.enset.ormjava.cas_users.repositories;
+
+import ma.enset.ormjava.cas_users.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+}

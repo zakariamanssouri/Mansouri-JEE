348fe4cb588a4b09599b3873d38ea2813134fdeb
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-01 20:52:08 +0000

    modification des fichiers repositories

--- Mapping-Objet-Relationnel/src/main/java/ma/enset/ormjava/cas_users/repositories/RoleRepository.java
+++ Mapping-Objet-Relationnel/src/main/java/ma/enset/ormjava/cas_users/repositories/RoleRepository.java
@@ -5,5 +5,5 @@ import ma.enset.ormjava.cas_users.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
+    Role findRoleByName(String rolename);
 }
--- Mapping-Objet-Relationnel/src/main/java/ma/enset/ormjava/cas_users/repositories/UserRepository.java
+++ Mapping-Objet-Relationnel/src/main/java/ma/enset/ormjava/cas_users/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import ma.enset.ormjava.cas_users.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
-
+    User findUserByUsername(String username);
 }

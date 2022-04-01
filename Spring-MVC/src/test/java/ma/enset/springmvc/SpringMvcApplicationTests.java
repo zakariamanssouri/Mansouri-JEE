c3ce8c734adfc0cbab7eb99d36d44ab3ba612b34
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-01 23:30:05 +0000

    Création du projet spring mvc pour la gestion des patients à travers une application web

---
+++ Spring-MVC/src/test/java/ma/enset/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

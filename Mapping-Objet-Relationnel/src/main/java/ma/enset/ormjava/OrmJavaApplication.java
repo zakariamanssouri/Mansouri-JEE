19d53c90572ed4fa5d79280a8ea8cd72426710a7
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-01 16:41:32 +0000

    Création du projet pour le mapping objet relationnel

---
+++ Mapping-Objet-Relationnel/src/main/java/ma/enset/ormjava/OrmJavaApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.ormjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmJavaApplication.class, args);
+	}
+
+}

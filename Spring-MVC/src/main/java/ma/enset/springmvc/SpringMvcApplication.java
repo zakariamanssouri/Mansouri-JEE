4dc7ed771bf3a611a9f7a6311d2e520a645db7af
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-02 00:03:12 +0000

    l'Ajout du code qui sera exécuté au démarrage de l'application

--- Spring-MVC/src/main/java/ma/enset/springmvc/SpringMvcApplication.java
+++ Spring-MVC/src/main/java/ma/enset/springmvc/SpringMvcApplication.java
@@ -1,7 +1,13 @@
 package ma.enset.springmvc;
 
+import ma.enset.springmvc.entites.Patient;
+import ma.enset.springmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringMvcApplication {
@@ -10,4 +16,25 @@ public class SpringMvcApplication {
 		SpringApplication.run(SpringMvcApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+		return args -> {
+			patientRepository.save(
+					new Patient(null, "Hassan", new Date(), false,10)
+			);
+			patientRepository.save(
+					new Patient(null, "karim", new Date(), true,300)
+			);
+			patientRepository.save(
+					new Patient(null, "salim", new Date(), false,40)
+			);
+			patientRepository.save(
+					new Patient(null, "ahmed", new Date(), false,99)
+			);
+			patientRepository.findAll().forEach(patient -> {
+				System.out.println(patient.getId()+" "+patient.getNom());
+			});
+
+		};
+	}
 }

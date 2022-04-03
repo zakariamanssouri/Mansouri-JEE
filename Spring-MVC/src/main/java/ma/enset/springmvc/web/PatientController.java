4c223dc111af5af3228315078944d121bdd49395
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-03 00:28:13 +0000

    modification du PatientController

--- Spring-MVC/src/main/java/ma/enset/springmvc/web/PatientController.java
+++ Spring-MVC/src/main/java/ma/enset/springmvc/web/PatientController.java
@@ -4,9 +4,13 @@ import lombok.AllArgsConstructor;
 import ma.enset.springmvc.entites.Patient;
 import ma.enset.springmvc.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -17,11 +21,42 @@ public class PatientController {
     private final PatientRepository patientRepository;
 
     @GetMapping(path = "/index")
-    public String patients(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patientsList", patients);
+    public String patients(Model model,
+                           @RequestParam(name = "page", defaultValue = "0") int page,
+                           @RequestParam(name = "size", defaultValue = "10") int size,
+                           @RequestParam(name = "keyword", defaultValue = "") String keyword) {
+
+        Page<Patient> patients = patientRepository.findByNomContains(keyword,PageRequest.of(page, size));
+
+        model.addAttribute("patientsList", patients.getContent());
+        model.addAttribute("pages", new int[patients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyword);
+
         return "patients";
     }
+
+    @GetMapping("/delete")
+    public String delete(Long id,int page,String keyword){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
+
+    //if we are not going to use thymleaf
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> ListPatients() {
+        return patientRepository.findAll();
+    }
+
+
+
 
 
 }

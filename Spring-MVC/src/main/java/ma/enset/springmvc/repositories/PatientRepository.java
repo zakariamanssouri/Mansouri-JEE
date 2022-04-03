bd25f7cdb35043d403c580b8a9a923e324965bf0
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-03 00:31:15 +0000

    l'Ajout d'une méthode qui retourne la liste des patients selon un mot clé

--- Spring-MVC/src/main/java/ma/enset/springmvc/repositories/PatientRepository.java
+++ Spring-MVC/src/main/java/ma/enset/springmvc/repositories/PatientRepository.java
@@ -1,8 +1,11 @@
 package ma.enset.springmvc.repositories;
 
 import ma.enset.springmvc.entites.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains(String keyword,Pageable pageable);
 }

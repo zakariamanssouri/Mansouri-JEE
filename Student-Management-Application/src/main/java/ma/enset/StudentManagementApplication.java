f0ca7dd5f5d0279b7ebf737e7629b53ba633b905
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-16 20:19:12 +0000

    add passwordencoder bean & add instructions for creating two users with their roles

--- Student-Management-Application/src/main/java/ma/enset/StudentManagementApplication.java
+++ Student-Management-Application/src/main/java/ma/enset/StudentManagementApplication.java
@@ -1,7 +1,13 @@
 package ma.enset;
 
+import ma.enset.security.entities.AppUser;
+import ma.enset.security.service.SecurityService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class StudentManagementApplication {
@@ -10,4 +16,22 @@ public class StudentManagementApplication {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	//@Bean
+	CommandLineRunner commandLineRunner(SecurityService securityService) {
+		return args -> {
+			securityService.saveNewUser("zakaria", "1234", "1234");
+			securityService.saveNewUser("admin", "admin", "admin");
+			securityService.saveNewRole("ADMIN", "");
+			securityService.saveNewRole("USER", "");
+
+			securityService.addRoleToUser("zakaria","USER");
+			securityService.addRoleToUser("admin","USER");
+			securityService.addRoleToUser("admin","ADMIN");
+		};
+	}
 }
